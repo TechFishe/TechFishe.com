a2f14f2f8fbe9c8d3e984dd1b7887be3200f0420
TechFishe <jacobb2008@icloud.com>
2022-05-27 21:41:40 -0500

    Removed Unused Code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 //Copyright (c) TechFishe 2022
 import styles from '../styles/Home-Page/Home.module.css'
-//import Head from "next/dist/shared/lib/head"
-//import Script from "next/script"
 
 export default function Home(){
     return(
