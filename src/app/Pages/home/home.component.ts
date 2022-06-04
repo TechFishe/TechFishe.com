59f292399a7263ac81de6945fd21ed548fc008de
TechFishe <jacobb2008@icloud.com>
2022-06-04 17:17:57 -0500

    First Commit!

---
+++ src/app/Pages/home/home.component.ts
@@ -0,0 +1,12 @@
+//Copyright (c) TechFishe 2022
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent{
+
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+// Copyright (c) TechFishe 2022
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutMeComponent } from './Pages/about-me/about-me.component';
+import { WildCardComponent } from './Pages/wild-card/wild-card.component';
+
+const routes: Routes = [
+  {
+    path: 'Home', 
+    title: 'Home', 
+    component: HomeComponent
+  },
+  {
+    path: 'About-Me',
+    title: 'About Me',
+    component: AboutMeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/Home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**', 
+    title: 'Page Not Found',
+    component: WildCardComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
