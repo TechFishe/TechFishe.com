4fe67159ab4afe633d044c93e2d5b1dac33ac747
TechFishe <jacobb2008@icloud.com>
2022-12-04 12:00:50 -0600

    Fixed About & Home page

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -7,7 +7,7 @@
     <title>TechFishe.com | About</title>
 </svelte:head>
 
-<main class="flex w-screen place-content-center">
+<main class="flex w-screen place-content-center max-w-[105rem] min-w-[92rem] absolute right-0">
     <div class="absolute top-16">
         <h1 class="text-3xl font-bold tracking-wide text-center">About!</h1>
         <h3 class="text-2xl p-2 font-semibold tracking-wide">About me</h3>
